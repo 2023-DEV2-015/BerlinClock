16eaae17f2a4e5406671f3d1271d8df9933bf045
JSCMovify <julien.schoenaers@movify.com>
2023-02-07 10:21:42 +0100

    CREATE -- Addition of viewModel containing simple enum class corresponding to the color code that will be used later.

---
+++ app/src/main/java/com/example/berlinclock/viewModel/BerlinClockViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.berlinclock.viewModel
+
+import androidx.compose.ui.graphics.Color
+
+class BerlinClockViewModel {
+
+    enum class LightColors(val color : Color) {
+        OFF(Color.Transparent),
+        YELLOW(Color.Yellow),
+        RED(Color.Red)
+    }
+
+
+
+}
