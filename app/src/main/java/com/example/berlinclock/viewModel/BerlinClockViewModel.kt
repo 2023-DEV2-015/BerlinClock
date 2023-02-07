f4f7114637c50b2d046efe8466921c9dfc0c8929
JSCMovify <julien.schoenaers@movify.com>
2023-02-07 11:44:52 +0100

    ADD -- Addition of a timer in the view model and starting it on init of the viewModel and updating a variable for later use

--- app/src/main/java/com/example/berlinclock/viewModel/BerlinClockViewModel.kt
+++ app/src/main/java/com/example/berlinclock/viewModel/BerlinClockViewModel.kt
@@ -1,14 +1,30 @@
 package com.example.berlinclock.viewModel
 
+import android.os.CountDownTimer
 import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.berlinclock.models.BerlinTime
+import java.text.SimpleDateFormat
+import java.util.*
 
+
+const val TIME_FORMAT = "HH:mm:ss"
+const val DURATION = 600000L
+const val INTERVAL = 1000L
 class BerlinClockViewModel {
+
+
+    init{
+        startTime()
+    }
 
     private var _berlinTime : MutableLiveData<BerlinTime> = MutableLiveData(BerlinTime.init())
     val berlinTime: LiveData<BerlinTime> = _berlinTime
+    private lateinit var timer: CountDownTimer
+
+    private var _time = MutableLiveData<String>()
+    val time: LiveData<String> = _time
 
     enum class LightColors(val color : Color) {
         OFF(Color.Transparent),
@@ -20,6 +36,16 @@ class BerlinClockViewModel {
         _berlinTime.postValue(BerlinTime.init())
     }
 
-
-
+    private fun startTime() {
+        timer = object : CountDownTimer(DURATION, INTERVAL) {
+            override fun onTick(millisUntilFinished: Long) = updateClockTime()
+            override fun onFinish() {
+                start()
+            }
+        }
+        timer.start()
+    }
+    private fun updateClockTime() {
+        val time: String = SimpleDateFormat(TIME_FORMAT, Locale.getDefault()).format(Date())
+    }
 }
