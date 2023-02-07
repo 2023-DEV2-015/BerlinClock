2310c0f4cdf6e26c04663b4e990bf77e6ce4ee3a
JSCMovify <julien.schoenaers@movify.com>
2023-02-07 13:08:23 +0100

    MODIFY -- Edit the activity class in order to have a functional UI

--- app/src/main/java/com/example/berlinclock/MainActivity.kt
+++ app/src/main/java/com/example/berlinclock/MainActivity.kt
@@ -3,16 +3,21 @@ package com.example.berlinclock
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.berlinclock.ui.composable.BerlinClockUI
 import com.example.berlinclock.ui.theme.BerlinClockTheme
+import com.example.berlinclock.viewModel.BerlinClockViewModel
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: BerlinClockViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,22 +27,12 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    BerlinClockUI(viewModel)
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    BerlinClockTheme {
-        Greeting("Android")
-    }
-}
+
