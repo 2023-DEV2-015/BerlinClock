070db92d83c0be53dbf5942588037858746302b0
JSCMovify <julien.schoenaers@movify.com>
2023-02-07 13:06:18 +0100

    ADD -- implementation of dependency to observe the viewModel value addition of the time on the bottom of the screen

--- app/src/main/java/com/example/berlinclock/ui/composable/BerlinClockUI.kt
+++ app/src/main/java/com/example/berlinclock/ui/composable/BerlinClockUI.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -17,7 +18,7 @@ import com.example.berlinclock.viewModel.BerlinClockViewModel
 @Composable
 fun BerlinClockUI(viewModel: BerlinClockViewModel) {
     Column(verticalArrangement = Arrangement.Center) {
-        var clock = BerlinTime.init()
+        val clock : BerlinTime by viewModel.berlinTime.observeAsState(BerlinTime.init())
         Seconds(color = clock.secondsLight)
         clock.hoursLights.let {
             LightsRow(colorList = it.topRow)
@@ -27,6 +28,10 @@ fun BerlinClockUI(viewModel: BerlinClockViewModel) {
             LightsRow(colorList = it.topRow)
             LightsRow(colorList = it.bottomRow)
         }
+        val time : String by viewModel.time.observeAsState("00:00:00")
+        Text(text = time, modifier = Modifier
+            .padding(0.dp, 8.dp)
+            .align(alignment = Alignment.CenterHorizontally))
     }
 }
 
