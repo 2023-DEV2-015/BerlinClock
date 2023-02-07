867a38053ae857d11b847d8e818de2e8413a8ff9
JSCMovify <julien.schoenaers@movify.com>
2023-02-07 11:20:24 +0100

    CREATE -- modifying the composable in order to get a correct display

--- app/src/main/java/com/example/berlinclock/ui/composable/Seconds.kt
+++ app/src/main/java/com/example/berlinclock/ui/composable/Seconds.kt
@@ -1,13 +1,35 @@
 package com.example.berlinclock.ui.composable
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color.Companion.Black
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.berlinclock.viewModel.BerlinClockViewModel
 
 
 @Composable
 fun Seconds(color: BerlinClockViewModel.LightColors) {
-
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(((LocalConfiguration.current.screenHeightDp - 40) / 5).dp),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Box(
+            modifier = Modifier
+                .size(((LocalConfiguration.current.screenHeightDp - 40) / 5).dp)
+                .clip(CircleShape)
+                .background(color.color)
+                .border(3.dp, Black, CircleShape)
+        )
+    }
 }
 
 @Preview
