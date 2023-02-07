f3afb8fe817e6e382afbc18aee39da537bacd9d7
JSCMovify <julien.schoenaers@movify.com>
2023-02-07 10:29:49 +0100

    CREATE -- Creation of top level method isEven to calculate if an int value is even or odd + implementation of that method in order to make the first test pass

--- app/src/main/java/com/example/berlinclock/BerlinClock.kt
+++ app/src/main/java/com/example/berlinclock/BerlinClock.kt
@@ -1,11 +1,20 @@
 package com.example.berlinclock
 
+import com.example.berlinclock.viewModel.BerlinClockViewModel.LightColors
+import com.example.berlinclock.viewModel.BerlinClockViewModel.LightColors.*
+
+fun Int.isEven() =  this % 2 == 0
+
 class BerlinClock {
+
 
     companion object {
 
-        fun getSeconds(seconds : Int){
-
+        fun getSeconds(seconds : Int): LightColors {
+            return if(seconds.isEven())
+                YELLOW
+            else
+                OFF
         }
 
         fun getMinutes(minutes : Int){
