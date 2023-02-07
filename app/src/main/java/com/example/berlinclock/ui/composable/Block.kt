a13c92608f8706e02ad0e534d54a7c9b8382469b
JSCMovify <julien.schoenaers@movify.com>
2023-02-07 11:24:17 +0100

    CREATE -- Create base block composable for the rows

---
+++ app/src/main/java/com/example/berlinclock/ui/composable/Block.kt
@@ -0,0 +1,36 @@
+package com.example.berlinclock.ui.composable
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.berlinclock.viewModel.BerlinClockViewModel.LightColors.YELLOW
+
+@Composable
+fun Block(color: Color, count : Int) {
+    Box(
+        modifier = Modifier
+            .height(((LocalConfiguration.current.screenHeightDp - 100) / 5).dp)
+            .width(((LocalConfiguration.current.screenWidthDp - 40) / count).dp)
+            .padding(2.dp, 0.dp)
+            .clip(RectangleShape)
+            .background(color)
+            .border(3.dp, Color.Black)
+    )
+}
+
+@Preview
+@Composable
+fun BlockPreview(){
+    Block(color = YELLOW.color, count = 4)
+}
