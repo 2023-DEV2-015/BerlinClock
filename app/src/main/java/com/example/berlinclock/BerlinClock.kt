13a822f38479e331716285efb0b07f5ec62a0a00
JSCMovify <julien.schoenaers@movify.com>
2023-02-07 10:23:58 +0100

    CREATE -- Addition of a working class that will calculate the rendering of the clock (will be used to redact all the base tests)

---
+++ app/src/main/java/com/example/berlinclock/BerlinClock.kt
@@ -0,0 +1,37 @@
+package com.example.berlinclock
+
+class BerlinClock {
+
+    companion object {
+
+        fun getSeconds(seconds : Int){
+
+        }
+
+        fun getMinutes(minutes : Int){
+
+        }
+
+        fun getHours(hours : Int){
+
+        }
+
+    }
+
+    fun getMinutesTopRow(minutes : Int){
+
+    }
+
+    fun getMinutesBottomRow(minutes :Int){
+
+    }
+
+    fun getHoursTopRow(minutes : Int){
+
+    }
+
+    fun getHoursBottomRow(minutes :Int){
+
+    }
+
+}
